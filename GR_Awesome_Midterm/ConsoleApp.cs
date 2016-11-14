eaafc631614f29a981821fca9e98210764353770
Doreen <ddtroyer@gmail.com>
2016-11-14 14:47:57 -0500

    Fix class. Add verification of user's answers.

--- GR_Awesome_Midterm/ConsoleApp.cs
+++ GR_Awesome_Midterm/ConsoleApp.cs
@@ -6,9 +6,9 @@ using System.Threading.Tasks;
 
 namespace GR_Awesome_Midterm
 {
-    class ConsoleApp
+    public class ConsoleApp
     {
-        static void Main(string[] args)
+        public void Answers()
         {
             Console.Write("List all books? (y/n)");
             string userAnswer = Console.ReadLine();
@@ -16,47 +16,58 @@ namespace GR_Awesome_Midterm
             {
                 //list all books
             }
-
-            Console.Write("Select search option (\"author\", \"title\", or \"keyword: \"");
-            string userChoice = Console.ReadLine();
-
-            if (userChoice.ToLower() == "author")
+            string userChoice;
+            do
             {
-                Console.Write("Please enter author's name: ");
-                string userAuthor = Console.ReadLine();
+                Console.Write("Select search option (\"author\", \"title\", or \"keyword: \"");
+                userChoice = Console.ReadLine();
+
+                if (userChoice.ToLower() == "author")
+                {
+                    Console.Write("Please enter author's name: ");
+                    string userAuthor = Console.ReadLine();                   
+                }
+                else if (userChoice.ToLower() == "title")
+                {
+                    Console.Write("Please enter title: ");
+                    string userTitle = Console.ReadLine();
+                    //todo return list of books
+                }
+                else if (userChoice.ToLower() == "")
+                {
+                    Console.Write("Please enter keyword: ");
+                    string userKeyword = Console.ReadLine();
+                    //todo return list of books
+                }
             }
-            else if (userChoice.ToLower() == "title")
+            while (userChoice.ToLower() != "author" || userChoice.ToLower() != "title" || userChoice.ToLower() != "keyword");
+
+            string checkOrReturn;
+            do
             {
-                Console.Write("Please enter title: ");
-                string userTitle = Console.ReadLine();
+                Console.Write("Would you like to check out or return a book? (\"check out\"/\"return\")");
+                checkOrReturn = Console.ReadLine();
+
+                if (checkOrReturn.ToLower() == "check out" || checkOrReturn.ToLower() == "checkout")
+                {
+                    Console.Write("Which book would you like to check out? ");
+                    //todo figure out how they choose which book. is the list numbered?
+                    string userBook = Console.ReadLine();
+                   
+                    //todo check to see book's availability
+
+                    //todo if available, run pool so that the book is now unavailable
+                    //todo set return date
+
+                    //todo if not, say unavailable and ask if they want a different one
+                    //todo continue until a book is available or they say no
+                }
+                else
+                {
+                    //todo return book to pool
+                }
             }
-            else
-            {
-                Console.Write("Please enter keyword: ");
-                string userKeyword = Console.ReadLine();
-            }
-            Console.Write("Would you like to check out or return a book? (\"check out\"/\"return\")");
-            string checkOrReturn = Console.ReadLine();
-
-            if (checkOrReturn.ToLower() == "check out")
-            {
-
-                Console.Write("Which book would you like to check out? ");
-                //todo figure out how they choose which book. is the list numbered?
-                string userBook = Console.ReadLine();
-
-                //todo check to see book's availability
-
-                //todo if available, run pool so that the book is now unavailable
-                //todo set return date
-
-                //todo if not, say unavailable and ask if they want a different one
-                //todo continue until a book is available or they say no
-            }
-            else
-            {
-                //todo return book to pool
-            }
+            while (checkOrReturn.ToLower() != "check out" || checkOrReturn.ToLower() != "return");
         }
     }
 }
